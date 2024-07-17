19150f6a9a8fe46839a4a5a0ba181712d7b85d10
Jordan Phillips <jordan.phillips.988@gmail.com>
2024-07-17 10:54:20 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { Box, Container } from "@mui/material";
+import Header from "./components/header/Header";
+import "./App.css";
+
+function App() {
+  return (
+    <Box component="div" id="app">
+      <Container
+        sx={{
+          backgroundColor: "common.white",
+          width: "376px",
+          height: "666px",
+          borderRadius: "3px",
+          boxShadow: "0 0 5px 0 rgba(0, 0, 0, .9)",
+        }}
+      >
+        <Header />
+
+        <Box component="div" sx={{ p: 2 }}>
+          Some activities should be here
+        </Box>
+      </Container>
+    </Box>
+  );
+}
+
+export default App;
