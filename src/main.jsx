87c6a9bcce36f33f1a219355d79595439f42f2c6
Jordan Phillips <jordan.phillips.988@gmail.com>
2024-07-18 15:15:44 -0600

    Wrap app with navigation provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { CallProvider } from "./providers/CallProvider";
+import { NavigationProvider } from "./providers/NavigationProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CallProvider>
-      <App />
-    </CallProvider>
+    <NavigationProvider>
+      <CallProvider>
+        <App />
+      </CallProvider>
+    </NavigationProvider>
   </React.StrictMode>
 );
