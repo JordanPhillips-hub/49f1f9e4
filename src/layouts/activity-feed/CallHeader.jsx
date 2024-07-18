b168f56fcaa968ccb37d6c0d78fa6ac9fe79dec0
Jordan Phillips <jordan.phillips.988@gmail.com>
2024-07-18 12:21:44 -0600

    Import use call context, Add on click functionality on archive button, Update props and prop types

--- src/layouts/activity-feed/CallHeader.jsx
+++ src/layouts/activity-feed/CallHeader.jsx
@@ -1,8 +1,11 @@
 import PropTypes from "prop-types";
 import { Button, Typography, Stack } from "@mui/material";
 import { formatDate } from "../../utils/dateUtils";
+import { useCallContext } from "../../hooks/api.hooks";
 
-export default function CallHeader({ date }) {
+export default function CallHeader({ date, id, is_archived }) {
+  const { updateCallArchive } = useCallContext();
+
   return (
     <Stack direction="row" justifyContent="space-between">
       <Typography variant="body2" sx={{ my: 1, textAlign: "center" }}>
@@ -12,6 +15,7 @@ export default function CallHeader({ date }) {
       <Button
         variant="text"
         disableRipple
+        onClick={() => updateCallArchive(id, !is_archived)}
         sx={{
           textTransform: "none",
           "&:hover": {
@@ -28,4 +32,6 @@ export default function CallHeader({ date }) {
 
 CallHeader.propTypes = {
   date: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  is_archived: PropTypes.bool.isRequired,
 };
