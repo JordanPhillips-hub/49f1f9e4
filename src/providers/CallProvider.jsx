70e98126a2e08a2216d052b8e8305af122438ea9
Jordan Phillips <jordan.phillips.988@gmail.com>
2024-07-18 12:23:54 -0600

    Create function to handle toggling call archive status

--- src/providers/CallProvider.jsx
+++ src/providers/CallProvider.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect, createContext } from "react";
 import PropTypes from "prop-types";
 import { getActivities } from "../api";
 
+import { Requests } from "../api";
+
 export const CallContext = createContext();
 
 export const CallProvider = ({ children }) => {
@@ -16,8 +18,20 @@ export const CallProvider = ({ children }) => {
       .finally(() => setIsLoading(false));
   }, []);
 
+  const updateCallArchive = (id, is_archived) => {
+    setActivities(
+      activities.map((activity) =>
+        activity.id === id ? { ...activity, is_archived } : activity
+      )
+    );
+
+    return Requests.patchCallArchive(id, is_archived);
+  };
+
   return (
-    <CallContext.Provider value={{ activities, isLoading, error }}>
+    <CallContext.Provider
+      value={{ activities, isLoading, error, updateCallArchive }}
+    >
       {children}
     </CallContext.Provider>
   );
