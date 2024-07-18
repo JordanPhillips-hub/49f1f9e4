7ecaadd689d3fc3494f77f8a6814e4e4720381ff
Jordan Phillips <jordan.phillips.988@gmail.com>
2024-07-18 15:16:16 -0600

    Create use navigation context hook

--- src/hooks/api.hooks.js
+++ src/hooks/api.hooks.js
@@ -1,4 +1,6 @@
 import { useContext } from "react";
 import { CallContext } from "../providers/CallProvider";
+import { NavigationContext } from "../providers/NavigationProvider";
 
 export const useCallContext = () => useContext(CallContext);
+export const useNavigationContext = () => useContext(NavigationContext);
