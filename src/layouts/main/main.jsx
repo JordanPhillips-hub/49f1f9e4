9c7027e32e2a1f0791ebe81c2d62a4f85ee6689b
Jordan Phillips <jordan.phillips.988@gmail.com>
2024-07-17 21:51:54 -0600

    Add disable ripple prop on archive button

--- src/layouts/main/main.jsx
+++ src/layouts/main/main.jsx
@@ -38,6 +38,7 @@ export default function Main() {
 
             <Button
               variant="text"
+              disableRipple
               sx={{
                 textTransform: "none",
                 "&:hover": {
