3b5aa7b1d1335fb4d8d130b821367c77e3bd9523
Jordan Phillips <jordan.phillips.988@gmail.com>
2024-07-18 15:23:28 -0600

    Add updateAllCallsArchive function & refactor fetching

--- src/providers/CallProvider.jsx
+++ src/providers/CallProvider.jsx
@@ -1,8 +1,7 @@
 import { useState, useEffect, createContext } from "react";
 import PropTypes from "prop-types";
-import { getActivities } from "../api";
-
 import { Requests } from "../api";
+import { useNavigationContext } from "../hooks/api.hooks";
 
 export const CallContext = createContext();
 
@@ -10,13 +9,18 @@ export const CallProvider = ({ children }) => {
   const [activities, setActivities] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
+  const { currentView } = useNavigationContext();
+  const fetchAndSetActivities = () =>
+    Requests.getActivities().then(setActivities);
 
-  useEffect(() => {
-    getActivities()
-      .then(setActivities)
-      .catch(setError)
-      .finally(() => setIsLoading(false));
-  }, []);
+  const updateAllCallsArchive = () => {
+    setActivities(
+      activities.map((activity) => ({
+        ...activity,
+        is_archived: currentView === "All Calls" ? true : false,
+      }))
+    );
+  };
 
   const updateCallArchive = (id, is_archived) => {
     setActivities(
@@ -28,9 +32,21 @@ export const CallProvider = ({ children }) => {
     return Requests.patchCallArchive(id, is_archived);
   };
 
+  useEffect(() => {
+    fetchAndSetActivities()
+      .catch(setError)
+      .finally(() => setIsLoading(false));
+  }, []);
+
   return (
     <CallContext.Provider
-      value={{ activities, isLoading, error, updateCallArchive }}
+      value={{
+        activities,
+        isLoading,
+        error,
+        updateCallArchive,
+        updateAllCallsArchive,
+      }}
     >
       {children}
     </CallContext.Provider>
